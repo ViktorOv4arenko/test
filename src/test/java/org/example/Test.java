ac05f4611aafac284a6c7d42ff8fed3a158c94d2
ViktorOv4arenko <116972112+ViktorOv4arenko@users.noreply.github.com>
2024-02-12 12:22:24 +0200

    test
    Signed-off-by: ViktorOv4arenko <116972112+ViktorOv4arenko@users.noreply.github.com>

--- src/test/java/org/example/Test.java
+++ src/test/java/org/example/Test.java
@@ -5,7 +5,7 @@ import com.google.common.io.Files;
 public class Test {
     public static void main (String[] arg) {
 
-        System.out.println("TEST3");
+        System.out.println("TEST5");
 
     }
 
